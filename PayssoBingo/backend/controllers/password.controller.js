792c5dbd5251ae9f3b0d043c0bbc97365f8bbd17
robra0 <ararsarobra@gamil.com>
2026-08-05 02:49:39 +0300

    fix unused imports, add netlify config

--- PayssoBingo/backend/controllers/password.controller.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-
-export const hashPassword = async (password) => {
-  return await bcrypt.hash(password, 10);
-};
-
-export const comparePassword = async (password, hashPassword) => {
-  return await bcrypt.compare(password, hashPassword);
-};
--- PayssoBingo/backend/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
-import { authenticateTokenMiddleware } from "../middleware/authenticateToken.middleware.js";
-const authRouter = express.Router();
-
-authRouter.post("/signup/user", register);
-authRouter.post("/login/user", login);
-
-export default authRouter;
