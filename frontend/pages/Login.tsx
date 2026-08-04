c70a721c175ec24256e5eb4a19d68cf96ec24274
robra0 <ararsarobra@gamil.com>
2026-08-05 02:46:17 +0300

    fix unused imports, add netlify config

--- frontend/pages/Login.tsx
+++ frontend/pages/Login.tsx
@@ -1,6 +1,5 @@
 import { Leaf, Mail, Lock, Eye, ArrowRight } from "lucide-react";
 import { useState } from "react";
-import type { ChangeEvent, MouseEvent } from "react";
 import { useLogin, type Credentials } from "../src/services/useLogin";
 const Login = () => {
   const [email, setEmail] = useState<string>("");
