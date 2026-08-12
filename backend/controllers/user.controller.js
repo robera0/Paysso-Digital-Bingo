7bf64073635e92bbd112d1dfa41f57a39ae11edc
robra0 <ararsarobra@gamil.com>
2026-08-12 23:57:08 +0300

    add:update user profile controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,3 +10,60 @@ export const getUser = async (req, res) => {
     return res.status(500).json({ message: err.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  const id = new mongoose.Types.ObjectId(req.user.id);
+  const { email, password, phone, fullName, username } = req.body;
+
+  if (!id)
+    return res.status(400).json({ success: false, error: "Missing id." });
+  try {
+    const updateData = {
+      email,
+      password,
+      phone,
+      fullName,
+      username,
+    };
+
+    // delete undefined values
+    Object.keys(updateData).forEach(
+      (key) =>
+        (updateData[key] === undefined || updateData[key] === "") &&
+        delete updateData[key],
+    );
+
+    const existingUser = await UserModel.findOne({
+      _id: { $ne: id }, // exclude the current user
+      $or: [...(email ? [{ email }] : []), ...(username ? [{ username }] : [])],
+    });
+
+    if (email && existingUser) {
+      if (existingUser.email === email) {
+        return res.status(409).json({
+          success: false,
+          error: "Email already exists.",
+        });
+      }
+
+      if (username && existingUser.username === username) {
+        return res.status(409).json({
+          success: false,
+          error: "Username already exists.",
+        });
+      }
+    }
+
+    const updateUser = await UserModel.findByIdAndUpdate(
+      id,
+      {
+        $set: updateData,
+      },
+      { returnDocument: "after" },
+    );
+
+    return res.status(200).json({ profile: updateUser });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUser } from "../controllers/user.controller.js";
+import { getUser, updateUser } from "../controllers/user.controller.js";
 import { authenticateTokenMiddleware } from "../middleware/authenticateToken.middleware.js";
 
 const profileRouter = express.Router();
 
 profileRouter.get("/profile", authenticateTokenMiddleware, getUser);
+profileRouter.put("/profile", authenticateTokenMiddleware, updateUser);
 
 export default profileRouter;
